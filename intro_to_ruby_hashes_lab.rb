bee5fb426615e6c02c8d1895170b7773f6ae6ac9
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-02 02:20:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,15 +15,13 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  
+  {:name => "Grace Hopper"
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+  
 end
